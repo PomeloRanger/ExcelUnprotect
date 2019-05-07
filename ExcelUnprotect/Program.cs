36872079dcf7f85e43a2be4ccd89e8cdfdd6e6d6
PomeloRanger <pomeloteemo@gmail.com>
2019-05-07 21:07:05 +0800

    Clean up 2 >_>

--- ExcelUnprotect/Program.cs
+++ ExcelUnprotect/Program.cs
@@ -10,65 +10,51 @@ namespace ExcelUnprotect
     {
         static void Main(string[] args)
         {
-            LoadDocument(args);
-        }
-
-        private static void LoadDocument(string[] document)
-        {
-            if (document.Length == 0)
+            if (args.Length == 0)
             {
                 Console.WriteLine("No file was detected. Please drag and drop your Excel Files");
-                Console.ReadLine();
             }
             else
             {
-                RemoveProtection(document);
-            }
-        }
-
-        private static void RemoveProtection(string[] directory)
-        {
-            foreach (var files in directory)
-            {
-                if (CheckValidExtension(files))
+                foreach (var files in args)
                 {
-                    Console.WriteLine("Unprotecting " + files);
-
-                    Directory.CreateDirectory(Path.GetTempPath() + "ExcelUnprotect");
-
-                    File.Copy(files, Path.GetTempPath() + "ExcelUnprotect\\excelworkbook.zip", true);
-
-                    //Extract to folder so we can edit the
-                    ZipFile.ExtractToDirectory(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook.zip", Path.GetTempPath() + "ExcelUnprotect\\excelworkbook", true);
-
-                    // In the directory /xl/workbook.xml if there is <workbookProtection> which will be there if workbook is under protection
-                    TryRemoveWorkbookProtection(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook\\xl\\workbook.xml");
-
-                    //In the directory /xl/worksheets for every worksheet we check for <sheetProtection>
-                    string[] worksheetPath = Directory.GetFiles(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook\\xl\\worksheets");
-                    foreach (var worksheet in worksheetPath)
+                    if (CheckValidExtension(files))
                     {
-                        TryRemoveWorksheetProtection(worksheet);
+                        RemoveProtection(files);
                     }
-
-                    //Compile them back into zip
-                    Console.WriteLine("Compiling into unprotected zip format");
-                    ZipFile.CreateFromDirectory(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook", Path.GetTempPath() + "ExcelUnprotect\\unprotectedworkbook.zip");
-
-                    //Save back to original file location with an added + _unprotected and change file extension to .xlsx
-                    Console.WriteLine("File is unprotected with the name " + files + "_unprotected.xlsx");
-                    File.Copy(Path.GetTempPath() + "ExcelUnprotect\\unprotectedworkbook.zip", files + "_unprotected.xlsx", true);
-
-                    //Delete folder from Temp path
-                    Console.WriteLine("Deleting Temp folder");
-                    Directory.Delete(Path.GetTempPath() + "ExcelUnprotect", true);
-                }
-                else
-                {
-                    Console.WriteLine("This file is not supported");
                 }
             }
+            Console.WriteLine("Please press any key to exit");
+            Console.ReadKey();
         }
+
+        private static void RemoveProtection(string files)
+        {
+            string tempPath = Path.GetTempPath() + "ExcelUnprotect";
+
+            Directory.CreateDirectory(tempPath);
+            File.Copy(files, tempPath + "\\excelworkbook.zip", true);
+
+            ZipFile.ExtractToDirectory(tempPath + @"\excelworkbook.zip", tempPath + @"\excelworkbook", true);
+            Console.WriteLine("Attempting to remove workbook protection");
+            TryRemoveWorkbookProtection(tempPath + @"\excelworkbook\xl\workbook.xml");
+
+            Console.WriteLine("Attempting to remove worksheet protection");
+            string[] worksheetPath = Directory.GetFiles(tempPath + @"\excelworkbook\xl\worksheets");
+            foreach (var worksheet in worksheetPath)
+            {
+                TryRemoveWorksheetProtection(worksheet);
+            }
+            ZipFile.CreateFromDirectory(tempPath + @"\excelworkbook", tempPath + @"\unprotectedworkbook.zip");
+
+            //Save back to original file location with an added + _unprotected and change file extension to .xlsx
+            Console.WriteLine("File is unprotected with the name " + files + "_unprotected.xlsx");
+            File.Copy(tempPath + @"\unprotectedworkbook.zip", files + "_unprotected.xlsx", true);
+
+            Console.WriteLine("Deleting Temp folder");
+            Directory.Delete(tempPath, true);
+        }
+
 
         private static bool CheckValidExtension(string file)
         {
@@ -79,36 +65,47 @@ namespace ExcelUnprotect
             return false;
         }
 
-        private static void TryRemoveWorksheetProtection(string filePath)
+        private static XmlDocument LoadDocument(string filePath)
         {
             XmlDocument document = new XmlDocument();
             document.Load(filePath);
+            return document;
+        }
 
-            foreach (XmlElement xmlElement in document.DocumentElement)
-            {
-                if (xmlElement.Name == "sheetProtection")
-                {
-                    Console.WriteLine("Found sheetProtection at " + Path.GetFileName(filePath));
-                    xmlElement.ParentNode.RemoveChild(xmlElement);
-                }
-            }
-            document.Save(filePath);
+        private static void TryRemoveWorksheetProtection(string filePath)
+        {
+            var doc = LoadDocument(filePath);
+            RemoveWorksheetProtection(doc);
+            doc.Save(filePath);
         }
 
         private static void TryRemoveWorkbookProtection(string filePath)
         {
-            XmlDocument document = new XmlDocument();
-            document.Load(filePath);
+            var doc = LoadDocument(filePath);
+            RemoveWorkbookProtection(doc);
+            doc.Save(filePath);
+        }
 
+        private static void RemoveWorksheetProtection(XmlDocument document)
+        {
+            foreach (XmlElement xmlElement in document.DocumentElement)
+            {
+                if (xmlElement.Name == "sheetProtection")
+                {
+                    xmlElement.ParentNode.RemoveChild(xmlElement);
+                }
+            }
+        }
+
+        private static void RemoveWorkbookProtection(XmlDocument document)
+        {
             foreach (XmlNode items in document.DocumentElement.ChildNodes)
             {
                 if (items.Name == "workbookProtection")
                 {
-                    Console.WriteLine("Found workbookProtection");
                     items.ParentNode.RemoveChild(items);
                 }
             }
-            document.Save(filePath);
         }
     }
 }
