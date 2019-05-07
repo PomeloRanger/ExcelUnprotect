7b81e3ea15ad9a1f846644ffd86ccdc1073a8f50
PomeloRanger <pomeloteemo@gmail.com>
2019-05-07 08:25:42 +0800

    Attempt more cleanup(?)

--- ExcelUnprotect/Program.cs
+++ ExcelUnprotect/Program.cs
@@ -22,47 +22,61 @@ namespace ExcelUnprotect
             }
             else
             {
-                foreach (var files in document)
+                RemoveProtection(document);
+            }
+        }
+
+        private static void RemoveProtection(string[] directory)
+        {
+            foreach (var files in directory)
+            {
+                if (CheckValidExtension(files))
                 {
-                    if (Path.GetExtension(files) == ".xlsx")
+                    Console.WriteLine("Unprotecting " + files);
+
+                    Directory.CreateDirectory(Path.GetTempPath() + "ExcelUnprotect");
+
+                    File.Copy(files, Path.GetTempPath() + "ExcelUnprotect\\excelworkbook.zip", true);
+
+                    //Extract to folder so we can edit the
+                    ZipFile.ExtractToDirectory(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook.zip", Path.GetTempPath() + "ExcelUnprotect\\excelworkbook", true);
+
+                    // In the directory /xl/workbook.xml if there is <workbookProtection> which will be there if workbook is under protection
+                    TryRemoveWorkbookProtection(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook\\xl\\workbook.xml");
+
+                    //In the directory /xl/worksheets for every worksheet we check for <sheetProtection>
+                    string[] worksheetPath = Directory.GetFiles(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook\\xl\\worksheets");
+                    foreach (var worksheet in worksheetPath)
                     {
-                        Console.WriteLine("Unprotecting " + files);
-
-                        Directory.CreateDirectory(Path.GetTempPath() + "ExcelUnprotect");
-
-                        File.Copy(files, Path.GetTempPath() + "ExcelUnprotect\\excelworkbook.zip", true);
-
-                        //Extract to folder so we can edit the
-                        ZipFile.ExtractToDirectory(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook.zip", Path.GetTempPath() + "ExcelUnprotect\\excelworkbook", true);
-
-                        // In the directory /xl/workbook.xml if there is <workbookProtection> which will be there if workbook is under protection
-                        TryRemoveWorkbookProtection(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook\\xl\\workbook.xml");
-
-                        //In the directory /xl/worksheets for every worksheet we check for <sheetProtection>
-                        string[] worksheetPath = Directory.GetFiles(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook\\xl\\worksheets");
-                        foreach (var worksheet in worksheetPath)
-                        {
-                            TryRemoveWorksheetProtection(worksheet);
-                        }
-
-                        //Compile them back into zip
-                        Console.WriteLine("Compiling into unprotected zip format");
-                        ZipFile.CreateFromDirectory(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook", Path.GetTempPath() + "ExcelUnprotect\\unprotectedworkbook.zip");
-
-                        //Save back to original file location with an added + _unprotected and change file extension to .xlsx
-                        Console.WriteLine("File is unprotected with the name " + files + "_unprotected.xlsx");
-                        File.Copy(Path.GetTempPath() + "ExcelUnprotect\\unprotectedworkbook.zip", files + "_unprotected.xlsx", true);
-
-                        //Delete folder from Temp path
-                        Console.WriteLine("Deleting Temp folder");
-                        Directory.Delete(Path.GetTempPath() + "ExcelUnprotect", true);
+                        TryRemoveWorksheetProtection(worksheet);
                     }
-                    else
-                    {
-                        Console.WriteLine("This file is not supported");
-                    }
+
+                    //Compile them back into zip
+                    Console.WriteLine("Compiling into unprotected zip format");
+                    ZipFile.CreateFromDirectory(Path.GetTempPath() + "ExcelUnprotect\\excelworkbook", Path.GetTempPath() + "ExcelUnprotect\\unprotectedworkbook.zip");
+
+                    //Save back to original file location with an added + _unprotected and change file extension to .xlsx
+                    Console.WriteLine("File is unprotected with the name " + files + "_unprotected.xlsx");
+                    File.Copy(Path.GetTempPath() + "ExcelUnprotect\\unprotectedworkbook.zip", files + "_unprotected.xlsx", true);
+
+                    //Delete folder from Temp path
+                    Console.WriteLine("Deleting Temp folder");
+                    Directory.Delete(Path.GetTempPath() + "ExcelUnprotect", true);
+                }
+                else
+                {
+                    Console.WriteLine("This file is not supported");
                 }
             }
+        }
+
+        private static bool CheckValidExtension(string file)
+        {
+            if (Path.GetExtension(file) == ".xlsx")
+            {
+                return true;
+            }
+            return false;
         }
 
         private static void TryRemoveWorksheetProtection(string filePath)
